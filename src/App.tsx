3bf78e3a18039eeb82cd32275cf5c8af8ed156b7
RBRibeiro00 <romuloribeirob@hotmail.com>
2022-12-29 17:39:56 -0300

    Implementando tela de login com chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 /*O react tem um importante papel de componentização para
 tornar o código mais limpo e reutilizável*/
 
-import { Layout } from "./components/Layout";
-import styled from 'styled-components';
-
-// Componentizando o css
-const Box = styled.div`
-  background-color: orange;
-  border-radius: 25px;
-  padding-left: 15px;
-`
+import {
+  ChakraProvider,
+  Input,
+  Box,
+  Center,
+  Button
+} from "@chakra-ui/react";
 
 function App() {
   return (
@@ -17,23 +15,22 @@ function App() {
     por exemplo uma div para agrupar outras divs, economizando processamento */
 
     <>  {/* pode usar uma tag vazia também: <></> representa o React.Fragment */}
-      <Layout>
-        <Box>
-          <h1>Faça o login</h1>
+      <ChakraProvider>
+        <Box minHeight='100vh' backgroundColor='#336EFF' padding='25px'>
+          <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px'>
+            <Center>
+              <h1>Login</h1>
+            </Center>
+            <Input placeholder="Email" />
+            <Input placeholder="Password" />
+            <Center>
+              <Button colorScheme='teal' size='sm' width='100%'>
+                Sign in
+              </Button>
+            </Center>
+          </Box>
         </Box>
-        <label htmlFor='emailInput'>
-          Email
-        </label>
-        <input id='emailInput' type='email' />
-
-        <label htmlFor='passwordInput'>
-          Senha
-        </label>
-        <input id='passwordInput' type='password' />
-        <button>
-          Entrar
-        </button>
-      </Layout>
+      </ChakraProvider>
     </>
   );
 }
